bd4b5b78548f62665000f83710214451fc0696b1
syphyr <syphyr@gmail.com>
2024-03-14 21:06:27 +0100

    Get default long press delay value from Android settings
    The default long press delay value is set within Android's Settings -->
    Accessibility --> Touch & hold delay, so use that as the default value
    in Heliboard as well.  Also, increase the max long press delay value to
    1500 so that it matches the max value in Android's settings.

--- app/src/main/java/helium314/keyboard/latin/settings/Settings.java
+++ app/src/main/java/helium314/keyboard/latin/settings/Settings.java
@@ -17,6 +17,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.view.ContextThemeWrapper;
 import android.view.inputmethod.EditorInfo;
+import android.view.ViewConfiguration;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -326,6 +327,11 @@ public final class Settings implements SharedPreferences.OnSharedPreferenceChang
 
     public static ToolbarMode readToolbarMode(final SharedPreferences prefs) {
         return ToolbarMode.valueOf(prefs.getString(PREF_TOOLBAR_MODE, Defaults.PREF_TOOLBAR_MODE));
+    }
+
+    public static int readDefaultKeyLongpressTimeout() {
+        final int default_longpress_key_timeout = ViewConfiguration.getLongPressTimeout();
+        return default_longpress_key_timeout;
     }
 
     public static int readHorizontalSpaceSwipe(final SharedPreferences prefs) {
